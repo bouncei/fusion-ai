695d5e81dc85876bec5d78a13b20bad8cf103319
Joshua Inyang <joshuainyang255@gmail.com>
2024-08-02 15:28:32 +0100

    commit

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,22 +1,9 @@
-/**
- * Landing Page component.
- *
- * This component renders the landing page of the application, including the navigation bar, hero section, content section, and footer.
- *
- * @example
- * <LandingPage />
- */
 import Footer from "@/components/Footer";
 import LandingContent from "@/components/LandingContent";
 import LandingHero from "@/components/LandingHero";
 import LandingNavBar from "@/components/LandingNavBar";
 import React from "react";
 
-/**
- * Landing Page component function.
- *
- * @returns {JSX.Element} The JSX element representing the landing page.
- */
 function LandingPage() {
   return (
     <div className="h-full flex flex-col">
